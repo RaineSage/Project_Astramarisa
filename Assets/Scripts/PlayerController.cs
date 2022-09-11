b40be8d97f6449cafeb782aee3c9b575b5b6f9cf
Raine <akita.neru@gmx.de>
2022-09-11 13:59:53 +0200

    PlayerController
    - Player script added for simple movement and jump
    
    - Tag "Floor" and Layer "Plattform" added
    - Unity Recorder added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float m_movementSpeed = 5f;
+    [SerializeField]
+    private float m_jumpForce = 10f;
+
+    private bool m_isGrounded = false;
+    private bool m_isInAir = true;
+
+    private Rigidbody2D m_rigidbody;
+
+    void Awake()
+    {
+        m_rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        Vector2 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        direction = direction.normalized * m_movementSpeed;
+
+        direction.y = m_rigidbody.velocity.y;
+        m_rigidbody.velocity = direction;
+
+        direction.y = 0;
+
+        Debug.Log(m_isGrounded);
+
+    }
+
+    private void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && m_isGrounded && !m_isInAir)
+        {
+            Debug.Log(m_isGrounded);
+            m_rigidbody.AddForce(Vector2.up * m_jumpForce, ForceMode2D.Impulse);
+
+            m_isGrounded = false;
+            m_isInAir = true;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D _col)
+    {
+        if (_col.gameObject.tag == "Floor")
+        {
+          m_isGrounded = true;
+          m_isInAir = false;
+        }
+    }
+}
